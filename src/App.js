5472618ee31f540e424a4fcb39d29c3ed0ff780a
vova-lutcev@yandex.ru <75414851+fortyone-41@users.noreply.github.com>
2021-07-07 00:07:56 +0700

    error deplot6

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       width: "300px",
       marginLeft: "-150px"
     }}>Данные загружаются...</p>))
-    const db = await axios.get("https://localhost:8080/data")
+    const db = await axios.get("https://stc-testtask.herokuapp.com:8080/data")
     setTimeout(() => { setData(prev => ({ ...prev, data: db.data, active: "enabled" })) }, 500)
 
   }
