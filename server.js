02d71b3eb5939fd128c576f4695a5125183309e4
vova-lutcev@yandex.ru <75414851+fortyone-41@users.noreply.github.com>
2021-07-06 22:43:17 +0700

    error deploy

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.get('/data', cors(corsOptions), (req, res) => {
     host: "pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "h36iy87qw1o1etf3",
     database: "gwmnknro0kno2yef",
-    password: "ch0gjtch3rtgncse",
+    password: "ch0gjtch3rtgncse", 
     port: "3306"
   });
    
   connection.query("SELECT * FROM tasks",
-    function(results) {
+    function(err, results) {
       console.log(results)
       return res.json(results);
   });
