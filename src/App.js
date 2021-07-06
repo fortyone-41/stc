b53e6d3f0b1af0932ab4a58b65bd46d3777309d6
vova-lutcev@yandex.ru <75414851+fortyone-41@users.noreply.github.com>
2021-07-06 23:55:10 +0700

    error deplot4

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       width: "300px",
       marginLeft: "-150px"
     }}>Данные загружаются...</p>))
-    const db = await axios.get("https:/213.128.1.15:8080/data")
+    const db = await axios.get("https://213.128.1.15:8080/data")
     setTimeout(() => { setData(prev => ({ ...prev, data: db.data, active: "enabled" })) }, 500)
 
   }
